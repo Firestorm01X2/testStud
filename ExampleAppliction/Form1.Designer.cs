2edfef7410a6e2f67ee627b8720d0e50cd3a2b27
Firestorm01X2 <firestorm01x2@gmail.com>
2020-02-29 10:34:03 +0300

    добавили все файлы проекта

---
+++ ExampleAppliction/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ExampleAppliction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudN = new System.Windows.Forms.NumericUpDown();
+            this.btCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(12, 84);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(100, 20);
+            this.tbA.TabIndex = 0;
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(12, 28);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(100, 20);
+            this.tbB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Верхний предел(b)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Нижний предел(a)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "количество разбиений (N)";
+            // 
+            // nudN
+            // 
+            this.nudN.Location = new System.Drawing.Point(15, 169);
+            this.nudN.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.nudN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudN.Name = "nudN";
+            this.nudN.Size = new System.Drawing.Size(120, 20);
+            this.nudN.TabIndex = 5;
+            this.nudN.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btCalculate
+            // 
+            this.btCalculate.Location = new System.Drawing.Point(290, 108);
+            this.btCalculate.Name = "btCalculate";
+            this.btCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btCalculate.TabIndex = 6;
+            this.btCalculate.Text = "Расчитать";
+            this.btCalculate.UseVisualStyleBackColor = true;
+            this.btCalculate.Click += new System.EventHandler(this.btCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btCalculate);
+            this.Controls.Add(this.nudN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudN;
+        private System.Windows.Forms.Button btCalculate;
+    }
+}
+
---
+++ ExampleAppliction/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExampleAppliction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalculate_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(tbA.Text);
+            double b = Convert.ToDouble(tbB.Text);
+            long N = Convert.ToInt32(nudN.Value);
+        }
+    }
+}
